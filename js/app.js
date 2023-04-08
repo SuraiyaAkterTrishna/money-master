8b41d6239087f05882ae87bba45a37cb05c75710
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-08 21:58:08 +0600

    savings amount and remaining balance done

--- js/app.js
+++ js/app.js
@@ -4,19 +4,37 @@ function getInputFieldValueById(inputId){
     const inputFieldValue = parseInt(inputFieldValueString);
     return inputFieldValue;
 }
+
+function updateElementTextFieldById(elementId, value){
+    const elementField = document.getElementById(elementId);
+    elementField.innerText = value;
+}
+
+// expense
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    // income 
     const income = getInputFieldValueById('income');
-    // food, rent, clothes
     const food = getInputFieldValueById('food');
     const rent = getInputFieldValueById('rent');
     const clothes = getInputFieldValueById('clothes');
-    // total expense
-    const totalExpense = food + rent + clothes;
-    const totalExpenseElement = document.getElementById('total-expense');
-    totalExpenseElement.innerText = totalExpense;
-    // total balance 
-    const totalBalance = income - totalExpense;
-    const balanceField = document.getElementById('balance');
-    balanceField.innerText = totalBalance;
+
+    const expense = food + rent + clothes;
+    updateElementTextFieldById('total-expense', expense);
+    
+    const balance = income - expense;
+    updateElementTextFieldById('balance', balance);
+});
+
+// saving amount
+document.getElementById('btn-save').addEventListener('click', function(){
+    const income = getInputFieldValueById('income');
+    const saveRate = getInputFieldValueById('save');
+    const save = (saveRate * income) / 100;
+    updateElementTextFieldById('saving-amount', save);
+
+    const previousBalanceField = document.getElementById('balance');
+    const previousBalanceString = previousBalanceField.innerText;
+    const previousBalance = parseInt(previousBalanceString);
+    
+    const remainingBalance = previousBalance - save;
+    updateElementTextFieldById('remaining-balance', remainingBalance);
 });
